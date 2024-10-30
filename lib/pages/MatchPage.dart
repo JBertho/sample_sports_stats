3b8cd944c688fe8d5b59694b076792cfdc75b2d5
James BERTHO <james.bertho94@gmail.com>
2024-10-30 20:11:12 +0100

    update starting match

--- lib/pages/MatchPage.dart
+++ lib/pages/MatchPage.dart
@@ -43,8 +43,22 @@ class MatchPage extends StatelessWidget {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                   color: Colors.white,
-
                 ),
+                child: Padding(padding: EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  width: double.infinity,
+                height: 40,
+                decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                  color: AppColors.grey,
+                ),
+                  child: const Text("Nom de l'équipe adverse"),
+                ),
+                  ],
+                ),),
               )),
               
             ),
