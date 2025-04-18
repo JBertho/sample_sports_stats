a5416241df5e10428b75868430e03d20963c24ac
James Bertho <jbertho1@reseau-ges.fr>
2025-04-18 15:29:03 +0200

    [HISTORY] init stats

---
+++ lib/pages/histories/logic/HistoriesCubit.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:sample_sport_stats/infrastructure/DAO/game_dao.dart';
+import 'package:sample_sport_stats/models/Game.dart';
+import 'package:sample_sport_stats/models/Team.dart';
+
+import 'HistoriesState.dart';
+
+class HistoriesCubit extends Cubit<HistoriesState> {
+
+  final GameDAO gameDAO;
+
+  HistoriesCubit({required this.gameDAO}): super(InitTeamState());
+
+  void initHistories(Team? team) async {
+    if(team == null) {
+      return;
+    }
+    var gameEntities = await gameDAO.getGamesByTeamId(team.id!);
+    var games = gameEntities.map((gameEntity) => Game.fromEntity(gameEntity, team)).toList();
+    log("[DEBUGG GAMES] $games");
+    emit(HistoriesTeamState(games));
+
+  }
+
+  void historyPressed(Game history) {
+    emit(HistoryPressedState(state.games, history));
+    emit(HistoriesTeamState(state.games));
+  }
+}
